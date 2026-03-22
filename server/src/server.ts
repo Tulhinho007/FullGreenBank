caa8243576b6df6cf2169011875766a8e3b3a862
Tulhinho007 <kamaelcontatos@gmail.com>
2026-03-21 21:21:00 -0300

    security: implementar Content Security Policy (CSP)

--- server/src/server.ts
+++ server/src/server.ts
@@ -80,6 +80,15 @@ app.use((_req, res, next) => {
   // Impede execução de scripts não autorizados
   res.setHeader('X-XSS-Protection', '1; mode=block')
 
+  res.setHeader('Content-Security-Policy',
+  "default-src 'self'; " +
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+  "font-src 'self' https://fonts.gstatic.com; " +
+  "img-src 'self' data: blob:; " +
+  "connect-src 'self' https://*.supabase.co https://full-green-bank-backend.vercel.app; " +
+  "frame-ancestors 'none';"
+)
   next()
 })
 
